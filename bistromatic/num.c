dd1d790decf88881ccfe2bf1a65e013351263aab
Stephen Gardner <stephenbgardner@gmail.com>
2018-01-14 18:29:38 -0800

    Simplify read_digit

--- bistromatic/num.c
+++ bistromatic/num.c
@@ -6,7 +6,7 @@
 /*   By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 19:28:25 by sgardner          #+#    #+#             */
-/*   Updated: 2018/01/11 22:18:28 by sgardner         ###   ########.fr       */
+/*   Updated: 2018/01/14 15:22:33 by sgardner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,30 +84,11 @@ void	print_num(t_calc *calc, t_num *num)
 	free(out);
 }
 
-t_num	*read_num(t_calc *calc, char *raw, int len)
+t_bool	read_digit(t_calc *calc, t_num *num)
 {
-	t_num	*num;
 	char	*pos;
-	int		i;
 
-	if (!(num = (t_num *)ft_memalloc(sizeof(t_num))))
-		return (NULL);
-	i = 0;
-	num->sign = (i < len && raw[i] == '-') ? -1 : 1;
-	if (num->sign < 0)
-		i++;
-	while (i < len)
-	{
-		if (!(pos = ft_strchr(calc->base, raw[i++])))
-		{
-			syntax_error();
-			return (NULL);
-		}
-		if (!(append_digit(num, pos - calc->base)))
-		{
-			free(num);
-			return (NULL);
-		}
-	}
-	return (num);
+	if (!(pos = ft_strchr(calc->base, *calc->pos)))
+		return (syntax_error());
+	return (append_digit(num, pos - calc->base));
 }
