2d1e3be452e10d5fcbe00598603c0ca1c13b618c
Stephen Gardner <stephenbgardner@gmail.com>
2018-01-14 22:17:50 -0800

    Add function to peek at end of queue

--- bistromatic/util.c
+++ bistromatic/util.c
@@ -6,12 +6,17 @@
 /*   By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 21:45:17 by sgardner          #+#    #+#             */
-/*   Updated: 2018/01/12 15:10:18 by sgardner         ###   ########.fr       */
+/*   Updated: 2018/01/14 21:21:20 by sgardner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "bistro.h"
+
+void	*peek_end(t_queue *queue)
+{
+	return ((queue->end) ? queue->end->content : NULL);
+}
 
 t_num	*strip_zeroes(t_num *num)
 {
